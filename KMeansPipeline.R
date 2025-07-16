639c265430e8b5b08108cb8d6d026a1568857094
plambre0 <paololambre0@gmail.com>
2025-07-16 00:19:38 -0500

    Update KMeansPipeline.R
    added saving files

--- KMeansPipeline.R
+++ KMeansPipeline.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(rgl)
 library(hash)
 
+set.seed(1922)
 options(warn=-1)
 
 data_path <- readline(prompt = "Enter the path to the expression data: ")
@@ -12,6 +13,11 @@ planes_dict <- hash()
 planes_dict[["1"]] <- 'X'
 planes_dict[["2"]] <- 'Z'
 planes_dict[["3"]] <- 'Y'
+
+slice_dict <- hash() 
+slice_dict[["X"]] <- 'Coronal'
+slice_dict[["Z"]] <- 'Sagittal'
+slice_dict[["Y"]] <- 'Axial'
 
 cat("Select a plane:\n",
     "  1: Coronal\n",
@@ -46,10 +52,21 @@ genes_normalized <- processed_df %>%
 kmeans_raw <- kmeans(genes_raw,as.integer(k))
 kmeans_normalized <- kmeans(genes_normalized,as.integer(k))
 
+raw_clustered <- cbind(cluster = kmeans_raw$cluster,data[,c("voxRowNum", "Structure.ID", "X", "Y", "Z")],genes_raw)
+norm_clustered <- cbind(cluster = kmeans_normalized$cluster,data[,c("voxRowNum", "Structure.ID", "X", "Y", "Z")],genes_normalized)
+write.csv(raw_clustered, paste(slice_dict[[plane]],'_raw_clusters.csv',sep = ''))
+write.csv(norm_clustered, paste(slice_dict[[plane]],'_norm_clusters.csv',sep = ''))
+
 plot3D::scatter3D(data$Z,data$X,data$Y,col=kmeans_raw$cluster, colkey = FALSE, xlab = 'Z', ylab = 'X', zlab = 'Y')
 plot3D::scatter3D(processed_df$Z,processed_df$X,processed_df$Y,col=kmeans_normalized$cluster, colkey = FALSE, xlab = 'Z', ylab = 'X', zlab = 'Y')
 
+cat("Cluster results for ", slice_dict[[plane]], " Slice by percent similarity:","\n")
 cat("Structure ID's by clusters for raw data",'\n')
-table(Structures = data$Structure.ID,Clusters = kmeans_raw$cluster)
+results_raw <- table(Structures = data$Structure.ID,Clusters = kmeans_raw$cluster)
+print(results_raw)
 cat("Structure ID's by clusters for normalized data",'\n')
-table(Structures = data$Structure.ID,Clusters = kmeans_normalized$cluster)
+results_norm <- table(Structures = data$Structure.ID,Clusters = kmeans_normalized$cluster)
+print(results_norm)
+
+write.csv(results_raw,"Cluster_Struct_Table_Raw_Med_Imp.csv")
+write.csv(results_norm,"Cluster_Struct_Table_Norm_Med_Imp.csv")
